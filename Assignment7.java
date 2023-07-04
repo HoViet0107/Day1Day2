7b6dc7f1cff19b8cf34bf66d0c5bc1ed372d26ce
HoViet0107 <112065031+HoViet0107@users.noreply.github.com>
2023-07-04 20:52:03 +0700

    Update Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -5,21 +5,19 @@ import java.util.Scanner;
 public class Assignment7 {
 	
     static void DemKyTu(String str) {
-        int counter[] = new int[256];
+        int[] counter = new int[256];
         int len = str.length();
-        for (int i = 0; i < len; i++)
-            counter[str.charAt(i)]++;
-        char array[] = new char[str.length()];
+
         for (int i = 0; i < len; i++) {
-            array[i] = str.charAt(i);
-            int flag = 0;
-            for (int j = 0; j <= i; j++) {
-                if (str.charAt(i) == array[j])
-                    flag++;
+            char c = str.charAt(i);
+            counter[c]++;
+        }
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] > 0) {
+                char c = (char) i;
+                System.out.println("Số lần xuất hiện của " + c + " trong chuỗi: " + counter[i]);
             }
-            if (flag == 1)
-                System.out.println("Số lần xuất hiện của " + str.charAt(i)
-                        + " trong chuỗi:" + counter[str.charAt(i)]);
         }
     }
 
